e853786a26aa1bbd618321f159502e360b43d4d2
Jocelyn Lum <joce.lum@gmail.com>
2017-09-11 14:36:53 -0400

    moved all code from individual js files into general apps.js file

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,55 +1,119 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
-import FormOne from './FormOne';
-import FormTwo from './FormTwo';
-// import firebase from './firebase';
-// const dbRef = firebase.database().ref('/items');
+import  { toMoney } from './utilities'
+import firebase from './firebase';
+const dbRef = firebase.database().ref('/items');
 
+//form to enter budget
+class FormOne extends React.Component {
+render() {
+        return (
+            <section className="budget-section">
+                <form onSubmit={this.props.handleSubmit}>
+                    <label htmlFor="budget">What is your budget for this trip?</label>
+                    <input type="text" name="budget" placeholder="in CDN $" onChange={this.props.handleChange} value={this.props.budget}/>
+                    <button className="submit-btn">Submit</button>
+                </form>
+            </section>
+        );
+    }
+}
+
+//form to enter in individual expenses
+class FormTwo extends React.Component {
+    render() {
+        return (
+        <section className="addExpense">
+            <p>So, what did you just spend $ on?</p>
+            <form onSubmit={this.props.handleSubmit}> 
+               <div className="dropdown"> 
+                <select onChange={this.props.handleChange} value={this.props.category} name="category">
+                      <option key="Category" value="Category">Category</option>
+                      <option key="Food" value="Food">Food</option>
+                      <option key="Shopping" value="Shopping">Shopping</option>
+                      <option key="Transportation" value="Transportation">Transportation</option>
+                      <option key="Attraction" value="Attraction">Attractions</option>
+                      <option key="Other" value="Other">Other</option>
+                </select>
+                <i className="fa fa-chevron-down" aria-hidden="true"></i>
+                </div>
+                <input type="text" placeholder="Description" name="desc" onChange={this.props.handleChange} value={this.props.desc}/>
+                <input type="number" placeholder="Cost" name="cost" onChange={this.props.handleChange} value={this.props.cost}/>
+                <button className="add-btn">Add</button>
+            </form> 
+        </section>
+        )
+    }
+}
 
 class App extends React.Component {
 constructor() {
     super();
-    this.state = {
-        budget: '',
-        items: []
-    }
+        this.state = {
+            budget: '',
+            category: '',
+            desc: '',
+            cost: '',
+            items: [],
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 }    
     
-handleFormTwo(item) {
-    let items = this.state.items;
-    items.push(item);
-    this.setState([items: items]);
-}
+    handleSubmit(event) {
+        event.preventDefault();
+        let costItem = toMoney(this.state.cost);
+        //check to see if inputs are actually filled in
+        if (this.state.desc === '' || this.state.cost === '') {
+            alert('please add something!');
+        }  else {
+        const newItem = {
+            category: this.state.category,
+            desc: this.state.desc,
+            cost: costItem,
+        };
+        dbRef.push(newItem);
+        }
+    }
 
-handleFormOne(budget) {
-    let budgetAmt = this.state.budget;
-    console.log(this.state.budget);
-    this.setState(budgetAmt);
-}
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
 
-// componentDidMount() {
-//     //to retrieve data from Firebase:
-//     dbRef.on('value',(snapshot) => {
-//         const newItemsArray = [];
-//         const firebaseItems = snapshot.val();
-//         for (let key in firebaseItems) { 
-//             //"key" and "id" are made up, can be anything we want to call it!
-//             const firebaseItem = firebaseItems[key];
-//             firebaseItem.id = key; //this adds the id as well to our new array
-//             newItemsArray.push(firebaseItem);
-//         }
-//         this.setState({
-//             items: newItemsArray,
-//         });
-//     });
-// }
+componentDidMount() {
+    //to retrieve data from Firebase:
+    dbRef.on('value',(snapshot) => {
+        const newItemsArray = [];
+        const firebaseItems = snapshot.val();
+        for (let key in firebaseItems) { 
+            const firebaseItem = firebaseItems[key];
+                firebaseItem.id = key;
+            newItemsArray.push(firebaseItem);
+        }
+        this.setState({
+            items: newItemsArray,
+        });
+    });
+}
     render() {
       return (
        <div className="app wrapper"> 
         <Header />
-        <FormOne FormOne={this.handleFormOne.bind(this)} />
-        <FormTwo FormTwo={this.handleFormTwo.bind(this)} />
+        <FormOne
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+        budget={this.state.budget}
+        />
+        <FormTwo
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+        category={this.state.category}
+        desc={this.state.desc}
+        cost={this.state.cost}
+         />
         <section className="expenseList">
             <table>
         	  <thead>
@@ -60,8 +124,8 @@ handleFormOne(budget) {
             	 </tr>
         	  </thead>
         	  <tbody>      	  	
-            {this.state.items.map((item, index) => { 
-                return (<tr key={index}>
+            {this.state.items.map((item) => { 
+                return (<tr key={item.id}>
                         <td>{item.category}</td>
                         <td>{item.desc}</td>
                         <td>${item.cost}</td>
